6da0a3b73b4347f5244c6b3ca933ae0e25869011
Salim Hertelli <48710965+salim-hertelli@users.noreply.github.com>
2023-12-16 17:59:06 +0100

    Create your_own_risk.cpp

---
+++ comments/your_own_risk.cpp
@@ -0,0 +1,8 @@
+// Dear reader:
+// When I wrote this code, only god and
+// I knew how it worked.
+// Now, only god knows!
+//
+// Therefore, if you are trying to read
+// it, good luck and may god shares the
+// truth with you.
